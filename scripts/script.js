96be50ab7a2f235a282aec3bb5d3bfc0cb69e803
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-18 10:27:50 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -1,61 +1,73 @@
-const elForm = document.querySelector(".form-js");
+let name1 = "Akbar";
+console.log(name1);
+// const elForm = document.querySelector(".form-js");
 
-const elToDoInput = document.querySelector(".todo-input");
+// const elToDoInput = document.querySelector(".todo-input");
 
-const elToDoList = document.querySelector(".todo-list");
+// const elToDoList = document.querySelector(".todo-list");
 
-const elEmptyImage = document.querySelector(".empty-image");
+// const elEmptyImage = document.querySelector(".empty-image");
 
-const list = [];
+// const list = [];
 
-elForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  let value = elToDoInput.value;
-  const findIdx = list.findIndex((el) => el.title === value.trim());
-  if (value === "") {
-    alert("malumot mavjud emas");
-  } else if (findIdx < 0) {
-    const newObj = {
-      title: value.trim(),
-      id: Date.now(),
-    };
+// elForm.addEventListener("submit", function (evt) {
+//   evt.preventDefault();
+//   let value = elToDoInput.value;
+//   const findIdx = list.findIndex((el) => el.title === value.trim());
+//   if (value === "") {
+//     alert("malumot mavjud emas");
+//   } else if (findIdx < 0) {
+//     const newObj = {
+//       title: value.trim(),
+//       id: Date.now(),
+//     };
 
-    list.push(newObj);
-    elToDoInput.value = null;
-  } else {
-    alert("bu maluot mavjud");
-  }
+//     list.push(newObj);
+//     elToDoInput.value = null;
+//   } else {
+//     alert("bu maluot mavjud");
+//   }
 
-  rendiredFunc(list);
-});
+//   rendiredFunc(list);
+// });
 
-function remove(id) {
-  console.log(id);
-  const findIdx = list.findIndex((el) => el.id == id);
-  list.splice(findIdx, 1);
-  rendiredFunc(list);
-}
+// function remove(id) {
+//   console.log(id);
+//   const findIdx = list.findIndex((el) => el.id == id);
+//   list.splice(findIdx, 1);
+//   rendiredFunc(list);
+// }
 
-function rendiredFunc(arr) {
-  elToDoList.innerHTML = null;
+// function rendiredFunc(arr) {
+//   elToDoList.innerHTML = null;
 
-  arr.forEach((el) => {
-    elToDoList.innerHTML += `
-      <li class="todo">
-          <span class="checked">${el.title}</span>
-          <button
-          class="delete-btn"
-          onclick="remove(this)"
-          >
-          Edit
-          </button>
-          <button
-          class="delete-btn"
-          onclick="remove(${el.id})"
-          >
-          Delete
-          </button>
-      </li>
-      `;
-  });
-}
+//   arr.forEach((el) => {
+//     elToDoList.innerHTML += `
+//       <li class="todo">
+//           <span class="checked">${el.title}</span>
+//           <button
+//           class="delete-btn"
+//           onclick="remove(this)"
+//           >
+//           Edit
+//           </button>
+//           <button
+//           class="delete-btn"
+//           onclick="remove(${el.id})"
+//           >
+//           Delete
+//           </button>
+//       </li>
+//       `;
+//   });
+// }
+
+var name = "Akbar";
+
+// console.log(d());
+
+// function f() {
+//   function d() {}
+
+//   console.log(d());
+// }

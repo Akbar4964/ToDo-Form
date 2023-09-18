859dd354db2b8b730c02c1834e363755b0564599
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-18 10:33:52 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,7 @@ const elEmptyImage = document.querySelector(".empty-image");
 const list = [];
 
 elForm.addEventListener("submit", function (evt) {
-//   evt.preventDefault();
+  evt.preventDefault();
 //   let value = elToDoInput.value;
 //   const findIdx = list.findIndex((el) => el.title === value.trim());
 //   if (value === "") {

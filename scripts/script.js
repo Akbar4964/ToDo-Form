a40329017d6b01256d1b1fcb381e010313f22d26
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-18 10:43:57 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -52,7 +52,7 @@ function rendiredFunc(arr) {
 //           onclick="remove(${el.id})"
 //           >
 //           Delete
-//           </button>
+          </button>
       </li>
       `;
   });

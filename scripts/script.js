38bba8a73cda9ecd33fa9836df639dd7ad99f098
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-19 14:24:17 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -28,30 +28,41 @@ elForm.addEventListener("submit", function (evt) {
   rendiredFunc(list);
 });
 
-function remove(id) {
-  console.log(id);
-  const findIdx = list.findIndex((el) => el.id == id);
-  list.splice(findIdx, 1);
-  rendiredFunc(list);
+function remove(evt) {
+  if (evt.target.matches(".btn-delete")) {
+    console.log("bu btn delete");
+  }
+  // console.log(id);
+  // const findIdx = list.findIndex((el) => el.id == id);
+  // list.splice(findIdx, 1);
+  // rendiredFunc(list);
 }
-const newElement1 = document.createElement("li");
-const newElement2 = document.createElement("button");
-const newElement3 = document.createElement("button");
-const newElement4 = document.createElement("div");
+
 function rendiredFunc(arr) {
   elToDoList.innerHTML = null;
   arr.forEach((el) => {
-    elToDoList.innerHTML += newElement1.textContent = `${el.title}`;
-    elToDoList.appendChild(newElement1.appendChild(newElement4));
-    newElement4.append(newElement2, newElement3);
+    const newElement1 = document.createElement("li");
+    const newElementSpan = document.createElement("span");
+    const newElement2 = document.createElement("button");
+    const newElement3 = document.createElement("button");
+    const newElement4 = document.createElement("div");
     newElement2.textContent = "Edit";
+    newElement2.classList.add("btn");
+    newElement1.classList.add("list-item");
     newElement3.textContent = "Delete";
-    newElement3.addEventListener("click", remove);
-    if (newElement1.length) {
-      elEmptyImage.remove();
-    }
+    newElement3.classList.add("btn-delete");
+    newElementSpan.textContent = el.title;
+    newElement4.append(newElement2, newElement3);
+    newElement1.append(newElement4, newElementSpan);
+    elToDoList.appendChild(newElement1);
+
+    // if (newElement1.length) {
+    //   elEmptyImage.remove();
+    // }
   });
 }
+
+elToDoList.addEventListener("click", remove);
 
 // const elForm = document.querySelector(".form-js");
 

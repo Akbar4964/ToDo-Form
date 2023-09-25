ffdcd07a1719601931245de44db730d3c784c689
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-25 13:25:17 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -43,7 +43,7 @@ function rendiredFunc(array) {
     const newElement1 = document.createElement("li");
     const newElementP = document.createElement("p");
     const newElement2 = document.createElement("button");
-    const newElement3 = document.createElement("button");
+    // const newElement3 = document.createElement("button");
     // const newElement4 = document.createElement("div");
     // elToDoList.appendChild(newElement1);
     // newElement1.append(newElementP, newElement4);

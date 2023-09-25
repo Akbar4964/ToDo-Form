906d7b4cc9118c1fd44e0e6eba210251f270e858
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-25 13:28:21 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -29,12 +29,12 @@ elForm.addEventListener("submit", function (evt) {
 });
 
 function remove(evt) {
-  if (evt.target.matches(".btn-delete")) {
+  // if (evt.target.matches(".btn-delete")) {
     // const id = evt.target.dataset.itemId;
     // const findIdx = list.findIndex((el) => el.id == id);
     // list.splice(findIdx, 1);
     // rendiredFunc(list);
-  }
+  // }
 }
 
 // function rendiredFunc(array) {

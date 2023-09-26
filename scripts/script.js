da2438cf316cedf65eb3afbb0028d447da1c412d
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-26 12:32:43 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -29,8 +29,7 @@ elForm.addEventListener("submit", function (evt) {
 });
 
 function remove(evt) {
-  // if (evt.target.matches(".btn-delete")) {
-  //   const id = evt.target.dataset.itemId;
+  // if (evt.target.matches(".btn-delete")) 
   const id = evt.target.dataset.itemId;
   const findIdx = list.findIndex((el) => el.id == id);
   list.splice(findIdx, 1);

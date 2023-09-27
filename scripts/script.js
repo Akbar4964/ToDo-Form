2d66af8285187907eb4d71e2d9be1f27c9e81a24
Akbar_Inf <akbarshuhratullayev@gmail.com>
2023-09-27 10:45:28 +0500

    added codes

--- scripts/script.js
+++ scripts/script.js
@@ -93,7 +93,7 @@ function rendiredFunc(array) {
       el.id
     } fill="#000000" width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <path class="heart ${
-        el.isLike ? "heart-aktive" : ""
+        el.isBookmark ? "heart-aktive" : ""
       } d="M12 20a1 1 0 0 1-.437-.1C11.214 19.73 3 15.671 3 9a5 5 0 0 1 8.535-3.536l.465.465.465-.465A5 5 0 0 1 21 9c0 6.646-8.212 10.728-8.562 10.9A1 1 0 0 1 12 20z"/>
     </svg>`;
     newElement5.classList = "heart";
